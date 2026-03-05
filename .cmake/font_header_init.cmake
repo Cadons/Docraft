59d7f911a2e5acba5304ec6c6571ba201beaa2ba
Matteo Cadoni <cadons@mail.umbranet.ovh>
2026-03-05 23:10:23 +0100

    Resolve "Built in font not found on Windows"

---
+++ .cmake/font_header_init.cmake
@@ -0,0 +1,8 @@
+if(NOT DEFINED OUTPUT_FILE)
+    message(FATAL_ERROR "OUTPUT_FILE not defined")
+endif()
+
+file(WRITE "${OUTPUT_FILE}" "/* Auto-generated fonts header */\n")
+file(APPEND "${OUTPUT_FILE}" "#pragma once\n")
+file(APPEND "${OUTPUT_FILE}" "#include \"utils/docraft_font_registry.h\"\n\n")
+file(APPEND "${OUTPUT_FILE}" "void docraft_register_fonts() {}\n\n")
